709266cf73e3403d2c5c401e37961a6a0546ee4c
QPAngelGlez <angel.gonzalez@questionpro.com>
2023-10-23 10:14:48 -0600

    Nuevo Proyecto API
    Subimos todos los archivos dentro de la rama

---
+++ Models/Contact.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CoreAPIQPro.Models
+{
+	public class Contact
+	{
+        public string? email { get; set; }
+        public string? url { get; set; }
+    }
+}
+
+ 
---
+++ Utilities/GetDataClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using CoreAPIQPro.Models;
+
+namespace CoreAPIQPro.Utilities
+{
+    public class GetDataClass
+    {
+        public List<Contact> GetContactsFile(string fileNme)
+        {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "JsonResponses/"+ fileNme);
+
+            if (File.Exists(filePath))
+            {
+                using (StreamReader r = new StreamReader(filePath))
+                {
+                    string jsonString = r.ReadToEnd();
+                    List<Contact> response = JsonConvert.DeserializeObject<List<Contact>>(jsonString);
+                    return response;
+                }
+            }
+            else
+            {
+                return new List<Contact>();
+            }
+        }
+    }
+}
